5b309b793f3fd4bfe8fa80849cc060676aa039e8
7LPdWcaW <7LPdWcaW@microgrowery>
2015-05-05 22:48:09 +0100

    Created initial models

---
+++ app/src/main/java/me/anon/model/Nutrient.java
@@ -0,0 +1,15 @@
+package me.anon.model;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author 
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+public class Nutrient
+{
+	private double npc; // nitrogen
+	private double ppc; // phosphorus
+	private double kpc; // potassium
+}
---
+++ app/src/main/java/me/anon/model/PlantStage.java
@@ -0,0 +1,16 @@
+package me.anon.model;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author 
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+public enum PlantStage
+{
+	GERMINATION,
+	VEGETATION,
+	FLOWER,
+	CURING;
+}
