dc53fb7c3d80269586e5d552c94e065b6d956f94
7LPdWcaW <7LPdWcaW@microgrowery>
2015-05-05 22:47:28 +0100

    Initial project structure

---
+++ app/src/main/java/me/anon/grow/MainApplication.java
@@ -0,0 +1,15 @@
+package me.anon.grow;
+
+import android.app.Application;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author 
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+public class MainApplication extends Application
+{
+
+}
