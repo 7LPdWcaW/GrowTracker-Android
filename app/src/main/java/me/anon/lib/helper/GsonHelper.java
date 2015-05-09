ab1fa5b17c96abd1098a90b8f41590ce9b66fd98
7LPdWcaW <7LPdWcaW@microgrowery>
2015-05-09 23:17:13 +0100

    Fixed issue with actions not saving correctly

---
+++ app/src/main/java/me/anon/lib/helper/GsonHelper.java
@@ -0,0 +1,94 @@
+package me.anon.lib.helper;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+
+import me.anon.model.Action;
+import me.anon.model.Feed;
+import me.anon.model.Water;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author 
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+public class GsonHelper
+{
+	public static <T> T parse(String json, Class<T> object)
+	{
+		return getGson().fromJson(json, object);
+	}
+
+	public static <T> T parse(String json, Type object)
+	{
+		return getGson().fromJson(json, object);
+	}
+
+	public static String parse(Object object)
+	{
+		return getGson().toJson(object);
+	}
+
+	public static Gson getGson()
+	{
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Action.class, new JsonDeserializer<Action>()
+		{
+			@Override public Action deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
+			{
+				JsonObject jsonObj = json.getAsJsonObject();
+				Gson g = new Gson();
+				Action action = null;
+
+				if (json.getAsJsonObject().get("type").getAsString().equals("Feed"))
+				{
+					action = g.fromJson(jsonObj, Feed.class);
+				}
+				else if (json.getAsJsonObject().get("type").getAsString().equals("Water"))
+				{
+					action = g.fromJson(jsonObj, Water.class);
+				}
+
+				return action;
+			}
+		});
+
+		builder.registerTypeAdapter(Action.class, new JsonSerializer<Action>()
+		{
+			@Override public JsonElement serialize(Action src, Type typeOfSrc, JsonSerializationContext context)
+			{
+				if (src instanceof Water)
+				{
+					Gson g = new Gson();
+					JsonObject jsonObj = (JsonObject)g.toJsonTree(src);
+					jsonObj.addProperty("type", "Water");
+
+					return jsonObj;
+				}
+				else if (src instanceof Feed)
+				{
+					Gson g = new Gson();
+					JsonObject jsonObj = (JsonObject)g.toJsonTree(src);
+					jsonObj.addProperty("type", "Feed");
+
+					return jsonObj;
+				}
+
+				return null;
+			}
+		});
+
+		return builder.create();
+	}
+}
