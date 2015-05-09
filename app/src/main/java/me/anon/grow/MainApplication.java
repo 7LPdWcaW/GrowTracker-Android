ee8b517671c327fd40c8d1a984720809605846a6
7LPdWcaW <7LPdWcaW@microgrowery>
2015-05-10 00:37:34 +0100

    Save when action/image is made and display last image in plant list

--- app/src/main/java/me/anon/grow/MainApplication.java
+++ app/src/main/java/me/anon/grow/MainApplication.java
@@ -2,6 +2,11 @@ package me.anon.grow;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import lombok.Getter;
 import me.anon.lib.manager.PlantManager;
 
 /**
@@ -13,10 +18,19 @@ import me.anon.lib.manager.PlantManager;
  */
 public class MainApplication extends Application
 {
+	@Getter private static DisplayImageOptions displayImageOptions;
+
 	@Override public void onCreate()
 	{
 		super.onCreate();
 
 		PlantManager.getInstance().initialise(this);
+
+		displayImageOptions = new DisplayImageOptions.Builder()
+			.cacheInMemory(true)
+			.cacheOnDisk(true)
+			.build();
+
+		ImageLoader.getInstance().init(new ImageLoaderConfiguration.Builder(this).build());
 	}
 }
