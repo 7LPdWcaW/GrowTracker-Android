6d56d733546e890b6f4d83645ec700d31336de47
7LPdWcaW <7LPdWcaW@gmail.com>
2015-07-30 17:20:37 +0100

    Replaced fab with action done

--- app/src/main/java/me/anon/grow/AddPlantActivity.java
+++ app/src/main/java/me/anon/grow/AddPlantActivity.java
@@ -1,7 +1,9 @@
 package me.anon.grow;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import lombok.experimental.Accessors;
 import me.anon.grow.fragment.PlantDetailsFragment;
@@ -24,6 +26,9 @@ public class AddPlantActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_done_white_24dp);
+
 		setContentView(R.layout.fragment_holder);
 		Views.inject(this);
 
@@ -31,5 +36,22 @@ public class AddPlantActivity extends AppCompatActivity
 		{
 			getFragmentManager().beginTransaction().replace(R.id.fragment_holder, PlantDetailsFragment.newInstance(-1), TAG_FRAGMENT).commit();
 		}
+	}
+
+	@Override public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if (item.getItemId() == android.R.id.home)
+		{
+			Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment_holder);
+
+			if (fragment instanceof PlantDetailsFragment)
+			{
+				((PlantDetailsFragment)fragment).save();
+			}
+
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
