e661a2bfe6c1b9471f98ee05407486b75b4fa462
7LPdWcaW <7LPdWcaW@gmail.com>
2015-08-13 12:56:20 +0100

    Added image taken date into image lightbox

--- app/src/main/java/me/anon/grow/fragment/ImageLightboxDialog.java
+++ app/src/main/java/me/anon/grow/fragment/ImageLightboxDialog.java
@@ -5,17 +5,23 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.Html;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 import me.anon.grow.R;
+import me.anon.lib.DateRenderer;
 import me.anon.lib.Views;
 
 @Views.Injectable
@@ -101,6 +107,21 @@ public class ImageLightboxDialog extends Activity
 			final View imageLayout = inflater.inflate(R.layout.image_lightbox_image, view, false);
 			loadImage(imageLayout, position);
 
+			try
+			{
+				DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(view.getContext());
+				DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(view.getContext());
+				String[] parts = images[position].split("/");
+				String date = parts[parts.length - 1].split("\\.")[0];
+
+				String dateStr = dateFormat.format(new Date(Long.parseLong(date))) + " " + timeFormat.format(new Date(Long.parseLong(date)));
+				((TextView)imageLayout.findViewById(R.id.taken)).setText(Html.fromHtml("<b>Image taken</b>: " + dateStr + " (" + new DateRenderer().timeAgo(Long.parseLong(date)).formattedDate + " ago)"));
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+
 			((ViewPager)view).addView(imageLayout, 0);
 			return imageLayout;
 		}
@@ -108,6 +129,7 @@ public class ImageLightboxDialog extends Activity
 		private void loadImage(final View v, final int position)
 		{
 			final SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)v.findViewById(R.id.image);
+			imageView.setDoubleTapZoomScale(1.5f);
 			imageView.setImage(ImageSource.uri("file://" + images[position]));
 		}
 
