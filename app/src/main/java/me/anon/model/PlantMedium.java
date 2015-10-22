8e068b3e83cd6b2fd524563df96634f461e934c9
7LPdWcaW <7LPdWcaW@microgrowery>
2015-10-22 15:47:32 +0100

    Added medium enum

---
+++ app/src/main/java/me/anon/model/PlantMedium.java
@@ -0,0 +1,34 @@
+package me.anon.model;
+
+import lombok.Getter;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author 7LPdWcaW
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+public enum PlantMedium
+{
+	SOIL("Soil"),
+	HYDRO("Hydroponics");
+
+	@Getter private String printString;
+
+	private PlantMedium(String name)
+	{
+		this.printString = name;
+	}
+
+	public static String[] names()
+	{
+		String[] names = new String[values().length];
+		for (int index = 0; index < names.length; index++)
+		{
+			names[index] = values()[index].getPrintString();
+		}
+
+		return names;
+	}
+}
