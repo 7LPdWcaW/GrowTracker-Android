a3c2c83c90567aa65dd0e62c82d38f41633e7f38
7LPdWcaW <7LPdWcaW@microgrowery>
2015-11-08 16:11:55 +0000

    Removed inport

--- app/src/main/java/me/anon/grow/BootActivity.java
+++ app/src/main/java/me/anon/grow/BootActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.text.TextUtils;
 import android.util.Base64;
 import android.widget.Toast;
 
