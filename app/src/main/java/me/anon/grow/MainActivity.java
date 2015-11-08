b961779ab7c1ff124b42b5508a07b955c89eea8a
7LPdWcaW <7LPdWcaW@gmail.com>
2015-11-08 12:41:02 +0000

    Moved existing settings functionality into settings fragment

--- app/src/main/java/me/anon/grow/MainActivity.java
+++ app/src/main/java/me/anon/grow/MainActivity.java
@@ -1,23 +1,14 @@
 package me.anon.grow;
 
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 import lombok.experimental.Accessors;
 import me.anon.grow.fragment.PlantListFragment;
 import me.anon.lib.Views;
-import me.anon.lib.helper.GsonHelper;
-import me.anon.lib.manager.PlantManager;
 
 /**
  * // TODO: Add class description
@@ -47,16 +38,7 @@ public class MainActivity extends AppCompatActivity
 
 	@Override public boolean onCreateOptionsMenu(Menu menu)
 	{
-		menu.add(1, 1, 1, "Readme");
-		menu.add(2, 2, 2, "Export data");
-		try
-		{
-			menu.add(3, 3, 3, "Version " + getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
-		}
-		catch (PackageManager.NameNotFoundException e)
-		{
-			e.printStackTrace();
-		}
+		menu.add(1, 1, 1, "Settings");
 
 		return super.onCreateOptionsMenu(menu);
 	}
@@ -65,38 +47,8 @@ public class MainActivity extends AppCompatActivity
 	{
 		if (item.getItemId() == 1)
 		{
-			String readme = "";
-
-			try
-			{
-				InputStream stream = new BufferedInputStream(getAssets().open("readme.html"), 8192);
-				int len = 0;
-				byte[] buffer = new byte[8192];
-
-				while ((len = stream.read(buffer)) != -1)
-				{
-					readme += new String(buffer, 0, len);
-				}
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-
-			new AlertDialog.Builder(this)
-				.setMessage(Html.fromHtml(readme))
-				.show();
-
-			return true;
-		}
-		else if (item.getItemId() == 2)
-		{
-			String json = GsonHelper.parse(PlantManager.getInstance().getPlants());
-
-			Intent share = new Intent(Intent.ACTION_SEND);
-			share.putExtra(Intent.EXTRA_TEXT, json);
-			share.setType("text/plain");
-			startActivity(share);
+			Intent settings = new Intent(this, SettingsActivity.class);
+			startActivity(settings);
 		}
 
 		return super.onOptionsItemSelected(item);
