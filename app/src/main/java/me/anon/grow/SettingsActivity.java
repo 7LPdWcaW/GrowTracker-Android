9305c2eb9d9fc36203154cb7038f5bfa764d975f
7LPdWcaW <7LPdWcaW@gmail.com>
2015-11-08 12:37:51 +0000

    Added settings activity/fragment

---
+++ app/src/main/java/me/anon/grow/SettingsActivity.java
@@ -0,0 +1,36 @@
+package me.anon.grow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import lombok.experimental.Accessors;
+import me.anon.grow.fragment.PlantListFragment;
+import me.anon.lib.Views;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author 7LPdWcaW
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+@Accessors(prefix = {"m", ""}, chain = true)
+public class SettingsActivity extends AppCompatActivity
+{
+	private static final String TAG_FRAGMENT = "current_fragment";
+
+	@Override protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.fragment_holder);
+		Views.inject(this);
+
+		setTitle("Settings");
+
+		if (getFragmentManager().findFragmentByTag(TAG_FRAGMENT) == null)
+		{
+			getFragmentManager().beginTransaction().replace(R.id.fragment_holder, new PlantListFragment(), TAG_FRAGMENT).commit();
+		}
+	}
+}
