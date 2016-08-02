a478b457484f9b08cd2f97a8f20f3b53e19b39bb
7LPdWcaW <7LPdWcaW@gmail.com>
2016-08-02 07:54:22 +0100

    Removed unnecessary fields from crash report

--- app/src/main/java/me/anon/model/CrashReport.java
+++ app/src/main/java/me/anon/model/CrashReport.java
@@ -12,17 +12,10 @@ public class CrashReport implements Serializable
 	private String packageName = "unknown";
 	private String versionCode = "0";
 
-	// User information
-	private String deviceId = "";
-	private String pushId = "";
-
 	// Device information
 	private String model = "unknown";
 	private String manufacturer = "unknown";
 	private String osVersion = "unknown";
-	private String screenSize = "0x0";
-	private String screenDensity = "0x0";
-	private String contentDensity = "1";
 
 	private Throwable exception;
 	private String additionalMessage = "";
