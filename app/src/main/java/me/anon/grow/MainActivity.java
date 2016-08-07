ffa1cc134665b75c36c23853a82a0607996404b7
7LPdWcaW <7LPdWcaW@microgrowery>
2016-08-07 18:40:21 +0100

    Added basic drawer layout

--- app/src/main/java/me/anon/grow/MainActivity.java
+++ app/src/main/java/me/anon/grow/MainActivity.java
@@ -1,10 +1,16 @@
 package me.anon.grow;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import lombok.experimental.Accessors;
 import me.anon.grow.fragment.PlantListFragment;
@@ -23,6 +29,9 @@ public class MainActivity extends AppCompatActivity
 {
 	private static final String TAG_FRAGMENT = "current_fragment";
 
+	@Views.InjectView(R.id.toolbar) private Toolbar toolbar;
+	@Views.InjectView(R.id.drawer_layout) private DrawerLayout drawer;
+
 	@Override protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
@@ -30,9 +39,35 @@ public class MainActivity extends AppCompatActivity
 		setContentView(R.layout.fragment_holder);
 		Views.inject(this);
 
+		setSupportActionBar(toolbar);
+		showDrawerToggle();
+
 		if (getFragmentManager().findFragmentByTag(TAG_FRAGMENT) == null)
 		{
 			getFragmentManager().beginTransaction().replace(R.id.fragment_holder, new PlantListFragment(), TAG_FRAGMENT).commit();
+		}
+	}
+
+	public void showDrawerToggle()
+	{
+		if (drawer != null)
+		{
+			ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, 0, 0)
+			{
+				@Override public void onDrawerSlide(View drawerView, float slideOffset)
+				{
+					super.onDrawerSlide(drawerView, slideOffset);
+
+					if (getCurrentFocus() != null)
+					{
+						InputMethodManager inputMethodManager = (InputMethodManager)MainActivity.this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+						inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+					}
+				}
+			};
+
+			drawer.setDrawerListener(drawerToggle);
+			drawerToggle.syncState();
 		}
 	}
 
