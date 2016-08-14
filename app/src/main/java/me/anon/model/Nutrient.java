f626b4521861a1902581432bb03564bf15534471
7LPdWcaW <7LPdWcaW@gmail.com>
2016-08-14 19:35:01 +0100

    Added back deprecated models for migration

---
+++ app/src/main/java/me/anon/model/Nutrient.java
@@ -0,0 +1,25 @@
+package me.anon.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author 7LPdWcaW
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+@Deprecated
+@Getter @Setter
+@Accessors(prefix = {"m", ""}, chain = true)
+public class Nutrient
+{
+	private Double npc; // nitrogen
+	private Double ppc; // phosphorus
+	private Double kpc; // potassium
+	private Double capc; // calcium
+	private Double spc; // sulfur
+	private Double mgpc; // magnesium
+}
