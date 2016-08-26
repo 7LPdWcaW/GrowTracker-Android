89ca6d0e25dc04c308327e595e1f837c4fb178f1
7LPdWcaW <7LPdWcaW@gmail.com>
2016-08-26 15:44:13 +0100

    Added permission helper class

---
+++ app/src/main/java/me/anon/lib/helper/PermissionHelper.java
@@ -0,0 +1,47 @@
+package me.anon.lib.helper;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.PermissionChecker;
+
+public class PermissionHelper
+{
+	@TargetApi(Build.VERSION_CODES.M)
+	public static boolean doPermissionCheck(final Fragment fragment, final String permission, final int requestCode, String dialogMessage)
+	{
+		if (PermissionChecker.checkSelfPermission(fragment.getActivity(), permission) != PackageManager.PERMISSION_GRANTED)
+		{
+			if (fragment.shouldShowRequestPermissionRationale(permission) && dialogMessage != null)
+			{
+				new AlertDialog.Builder(fragment.getActivity())
+					.setMessage(dialogMessage)
+					.setPositiveButton("OK", new DialogInterface.OnClickListener()
+					{
+						@Override public void onClick(DialogInterface dialog, int which)
+						{
+							fragment.requestPermissions(new String[]{permission}, requestCode);
+						}
+					})
+					.setNegativeButton("Cancel", null)
+					.show();
+
+				return false;
+			}
+
+			fragment.requestPermissions(new String[]{permission}, requestCode);
+			return false;
+		}
+
+		return true;
+	}
+
+	public static boolean hasPermission(Context context, String permission)
+	{
+		return Build.VERSION.SDK_INT < 23 || PermissionChecker.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
+	}
+}
