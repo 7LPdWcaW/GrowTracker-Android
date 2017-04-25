ba9b17121be43c92376a299b26d66f4c86b34c15
7LPdWcaW <7LPdWcaW@microgrowery>
2017-04-25 16:29:22 +0100

    Added basic plant request activity

---
+++ app/src/main/java/me/anon/grow/RequestActivity.java
@@ -0,0 +1,72 @@
+package me.anon.grow;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Html;
+import android.util.Base64;
+
+import me.anon.lib.helper.EncryptionHelper;
+import me.anon.lib.helper.GsonHelper;
+import me.anon.lib.manager.PlantManager;
+
+/**
+ * Class used for other applications to request data from this application
+ */
+public class RequestActivity extends Activity
+{
+	@Override protected void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		if (getIntent() != null)
+		{
+			try
+			{
+				String callingPackage = getCallingPackage();
+				CharSequence applicationLabel = getPackageManager().getApplicationLabel(getPackageManager().getApplicationInfo(callingPackage, 0));
+				Drawable applicationIcon = getPackageManager().getApplicationIcon(getPackageManager().getApplicationInfo(callingPackage, 0));
+
+				new AlertDialog.Builder(this)
+					.setIcon(applicationIcon)
+					.setMessage(Html.fromHtml("<b>" + applicationLabel + "</b> has requested a copy of the plant data"))
+					.setPositiveButton("Allow", new DialogInterface.OnClickListener()
+					{
+						@Override public void onClick(DialogInterface dialog, int which)
+						{
+							String plantListData = GsonHelper.parse(PlantManager.getInstance().getPlants());
+
+							if (MainApplication.isEncrypted())
+							{
+								plantListData = Base64.encodeToString(EncryptionHelper.encrypt(MainApplication.getKey(), plantListData), Base64.NO_WRAP);
+							}
+
+							Intent ret = new Intent();
+							ret.putExtra("me.anon.grow.PLANT_LIST", plantListData);
+							ret.putExtra("me.anon.grow.ENCRYPTED", MainApplication.isEncrypted());
+
+							setResult(RESULT_OK, ret);
+							finish();
+						}
+					})
+					.setNegativeButton("Deny", new DialogInterface.OnClickListener()
+					{
+						@Override public void onClick(DialogInterface dialog, int which)
+						{
+							finish();
+						}
+					})
+					.show();
+			}
+			catch (PackageManager.NameNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
