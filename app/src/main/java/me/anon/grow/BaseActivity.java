5f3cfa66e51d8b024f2a415c8ed94b3604339303
7LPdWcaW <7LPdWcaW@microgrowery>
2017-05-27 15:23:50 +0100

    Added check method for checking if we can decrypt

--- app/src/main/java/me/anon/grow/BaseActivity.java
+++ app/src/main/java/me/anon/grow/BaseActivity.java
@@ -2,12 +2,33 @@ package me.anon.grow;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
 /**
  * Base activity that sends broadcast events on resume
  */
 public class BaseActivity extends AppCompatActivity
 {
+	/**
+	 * Checks if we need to reauth for decrypting or not,
+	 * @return
+	 */
+	public boolean checkEncryptState()
+	{
+		// if we've resumed from death and we're in encrypted mode, we need to force re-auth
+		if (MainApplication.isEncrypted() && TextUtils.isEmpty(MainApplication.getKey()))
+		{
+			Intent intent = new Intent(this, BootActivity.class);
+			startActivity(intent);
+			finishAffinity();
+			System.exit(0);
+
+			return true;
+		}
+
+		return false;
+	}
+
 	@Override protected void onResume()
 	{
 		super.onResume();
