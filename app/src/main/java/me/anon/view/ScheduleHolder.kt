9970c5d506b94173580855105c752b3dc43ca25c
7LPdWcaW <7LPdWcaW@microgrowery>
2018-09-29 16:55:46 +0100

    Adds schedule holder binder

--- app/src/main/java/me/anon/view/ScheduleHolder.kt
+++ app/src/main/java/me/anon/view/ScheduleHolder.kt
@@ -2,6 +2,7 @@ package me.anon.view
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import kotlinx.android.synthetic.main.schedule_item.view.*
 import me.anon.model.FeedingSchedule
 
 /**
@@ -9,8 +10,12 @@ import me.anon.model.FeedingSchedule
  */
 class ScheduleHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 {
+	private val title = itemView.title
+	private val summary = itemView.summary
+
 	public fun bind(feedingSchedule: FeedingSchedule)
 	{
-
+		title.text = feedingSchedule.name
+		summary.text = feedingSchedule.description
 	}
 }
