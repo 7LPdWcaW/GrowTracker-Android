943bdf8582edd23b73430e731db452e20ce6de42
7LPdWcaW <7LPdWcaW@microgrowery>
2018-09-29 19:26:07 +0100

    Adds intent flow

--- app/src/main/java/me/anon/grow/fragment/FeedingScheduleListFragment.kt
+++ app/src/main/java/me/anon/grow/fragment/FeedingScheduleListFragment.kt
@@ -1,6 +1,7 @@
 package me.anon.grow.fragment
 
 import android.app.Fragment
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
@@ -10,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.LinearLayout.HORIZONTAL
 import kotlinx.android.synthetic.main.schedule_list_view.*
 import me.anon.controller.adapter.FeedingScheduleAdapter
+import me.anon.grow.FeedingScheduleDetailsActivity
 import me.anon.grow.R
 import me.anon.lib.manager.ScheduleManager
 
@@ -33,7 +35,7 @@ class FeedingScheduleListFragment : Fragment()
 		recycler_view.addItemDecoration(DividerItemDecoration(activity, HORIZONTAL))
 
 		fab_add.setOnClickListener {
-
+			startActivity(Intent(activity, FeedingScheduleDetailsActivity::class.java))
 		}
 	}
 }
