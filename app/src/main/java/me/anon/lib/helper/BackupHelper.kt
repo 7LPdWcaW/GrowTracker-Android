c2fe37e76ea34e19dd3a8f912621e4358209b353
7LPdWcaW <7LPdWcaW@microgrowery>
2019-05-18 15:15:12 +0100

    Prevent backup in failsafe mode and file extension for encrypted files

--- app/src/main/java/me/anon/lib/helper/BackupHelper.kt
+++ app/src/main/java/me/anon/lib/helper/BackupHelper.kt
@@ -1,6 +1,8 @@
 package me.anon.lib.helper
 
 import android.os.Environment
+import me.anon.grow.MainApplication
+import me.anon.lib.ext.T
 import me.anon.lib.manager.FileManager
 import me.anon.lib.manager.PlantManager
 import java.io.File
@@ -14,14 +16,18 @@ object BackupHelper
 	public var FILES_PATH = Environment.getExternalStorageDirectory().absolutePath + "/backups/GrowTracker"
 
 	@JvmStatic
-	public fun backupJson(): File
+	public fun backupJson(): File?
 	{
+		if (MainApplication.isFailsafe()) return null
+
+		val isEncrypted = MainApplication.isEncrypted()
 		val time = System.currentTimeMillis()
 		val backupPath = File(FILES_PATH)
+		val ext = isEncrypted T "dat" ?: "bak"
 		backupPath.mkdirs()
-		FileManager.getInstance().copyFile("${PlantManager.FILES_DIR}/plants.json", "$FILES_PATH/$time.plants.json.bak")
-		FileManager.getInstance().copyFile("${PlantManager.FILES_DIR}/schedules.json", "$FILES_PATH/$time.schedules.json.bak")
-		FileManager.getInstance().copyFile("${PlantManager.FILES_DIR}/gardens.json", "$FILES_PATH/$time.gardens.json.bak")
+		FileManager.getInstance().copyFile("${PlantManager.FILES_DIR}/plants.json", "$FILES_PATH/$time.plants.json.$ext")
+		FileManager.getInstance().copyFile("${PlantManager.FILES_DIR}/schedules.json", "$FILES_PATH/$time.schedules.json.$ext")
+		FileManager.getInstance().copyFile("${PlantManager.FILES_DIR}/gardens.json", "$FILES_PATH/$time.gardens.json.$ext")
 
 		return backupPath
 	}
