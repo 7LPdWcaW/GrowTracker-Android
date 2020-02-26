5cd676cf7683eb4e3c202147c896c81a2eec9cf0
7LPdWcaW <7LPdWcaW@gmail.com>
2020-02-26 13:07:38 +0000

    Moves common stats code out of local methods and adds additive chips

--- app/src/main/java/me/anon/grow/fragment/StatisticsFragment2.kt
+++ app/src/main/java/me/anon/grow/fragment/StatisticsFragment2.kt
@@ -16,8 +16,10 @@ import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.utils.ViewPortHandler
+import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.action_buttons_stub.*
 import kotlinx.android.synthetic.main.data_label_stub.view.*
+import kotlinx.android.synthetic.main.garden_action_buttons_stub.*
 import kotlinx.android.synthetic.main.statistics2_view.*
 import me.anon.grow.R
 import me.anon.lib.TdsUnit
@@ -37,6 +39,10 @@ import kotlin.math.roundToInt
  */
 class StatisticsFragment2 : Fragment()
 {
+	open class template()
+	open class header(var label: String) : template()
+	open class data(label: String, val data: String) : header(label)
+
 	companion object
 	{
 		@JvmStatic
@@ -56,6 +62,35 @@ class StatisticsFragment2 : Fragment()
 		}
 	}
 
+	// stat variables
+	val stageChanges by lazy {
+		plant.getStages().also {
+			it.toSortedMap(Comparator { first, second ->
+				(it[first]?.date ?: 0).compareTo(it[second]?.date ?: 0)
+			})
+		}
+	}
+
+	val plantStages by lazy {
+		plant.calculateStageTime().also {
+			it.remove(PlantStage.HARVESTED)
+		}
+	}
+
+	val aveStageWaters by lazy {
+		LinkedHashMap<PlantStage, ArrayList<Long>>().also { waters ->
+			waters.putAll(plantStages.keys.map { it }.associateWith { arrayListOf<Long>() })
+		}
+	}
+
+	val additives = arrayListOf<Additive>()
+	var endDate = System.currentTimeMillis()
+	var waterDifference = 0L
+	var lastWater = 0L
+	var totalWater = 0
+	var totalWaterAmount = 0.0
+	var totalFlush = 0
+
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
 		= inflater.inflate(R.layout.statistics2_view, container, false)
 
@@ -69,47 +104,13 @@ class StatisticsFragment2 : Fragment()
 
 		if (!::plant.isInitialized) return
 
+		calculateStats()
 		populateGeneralStats()
+		populateAdditiveStates()
 	}
 
-	private fun populateGeneralStats()
+	private fun calculateStats()
 	{
-		open class template()
-		open class header(var label: String) : template()
-		open class data(label: String, val data: String) : header(label)
-
-		val statTemplates = arrayListOf<template>()
-
-		stats_container.removeAllViews()
-		val plantStages = plant.calculateStageTime()
-		plantStages.remove(PlantStage.HARVESTED)
-		PlantStage.values().forEach { stage ->
-			if (plantStages.containsKey(stage))
-			{
-				plantStages[stage]?.let { time ->
-					statTemplates += data(
-						label = "${getString(stage.printString)}:",
-						data = "${TimeHelper.toDays(time).toInt()} ${resources.getQuantityString(R.plurals.time_day, TimeHelper.toDays(time).toInt())}"
-					)
-				}
-			}
-		}
-
-		val aveStageWaters = LinkedHashMap<PlantStage, ArrayList<Long>>()
-		aveStageWaters.putAll(plantStages.keys.map { it }.associateWith { arrayListOf<Long>() })
-		val stageChanges = plant.getStages()
-		stageChanges.toSortedMap(Comparator { first, second ->
-			(stageChanges[first]?.date ?: 0).compareTo(stageChanges[second]?.date ?: 0)
-		})
-
-		val startDate = plant.plantDate
-		var endDate = System.currentTimeMillis()
-		var waterDifference = 0L
-		var lastWater = 0L
-		var totalWater = 0
-		var totalWaterAmount = 0.0
-		var totalFlush = 0
-
 		plant.actions?.forEach { action ->
 			when (action)
 			{
@@ -126,6 +127,8 @@ class StatisticsFragment2 : Fragment()
 					// find the stage change where the date is older than the watering
 					val stage = stageChanges.filterValues { it.date <= action.date }.toSortedMap().lastKey()
 					aveStageWaters.getOrPut(stage, { arrayListOf<Long>() }).add(action.date)
+
+					additives.addAll(action.additives)
 				}
 
 				is EmptyAction -> {
@@ -133,7 +136,27 @@ class StatisticsFragment2 : Fragment()
 				}
 			}
 		}
+	}
 
+	private fun populateGeneralStats()
+	{
+		val statTemplates = arrayListOf<template>()
+
+		stats_container.removeAllViews()
+
+		PlantStage.values().forEach { stage ->
+			if (plantStages.containsKey(stage))
+			{
+				plantStages[stage]?.let { time ->
+					statTemplates += data(
+						label = "${getString(stage.printString)}:",
+						data = "${TimeHelper.toDays(time).toInt()} ${resources.getQuantityString(R.plurals.time_day, TimeHelper.toDays(time).toInt())}"
+					)
+				}
+			}
+		}
+
+		val startDate = plant.plantDate
 		val days = ((endDate - startDate) / 1000.0) * 0.0000115741
 
 		// total time
@@ -192,29 +215,7 @@ class StatisticsFragment2 : Fragment()
 				}
 			}
 
-		statTemplates.forEach { template ->
-			var dataView = when (template)
-			{
-				is data -> {
-					LayoutInflater.from(activity).inflate(R.layout.data_label_stub, stats_container, false).also {
-						it.label.text = template.label
-						it.data.text = template.data
-					}
-				}
-
-				is header -> {
-					LayoutInflater.from(activity).inflate(R.layout.subtitle_stub, stats_container, false).also {
-						(it as TextView).text = template.label
-						it.setPadding(0, resources.getDimension(R.dimen.padding_16dp).toInt(), 0, 0)
-					}
-				}
-
-				else -> null
-			}
-
-			dataView ?: return
-			stats_container += dataView
-		}
+		renderStats(stats_container, statTemplates)
 
 		// stage chart
 		val labels = arrayOfNulls<String>(plantStages.size)
@@ -280,5 +281,54 @@ class StatisticsFragment2 : Fragment()
 
 		stage_chart.legend.textColor = R.attr.colorOnSurface.resolveColor(context!!).toInt()
 		stage_chart.legend.isWordWrapEnabled = true
+	}
+
+	private fun populateAdditiveStates()
+	{
+		class additiveStat(var total: Double = 0.0, var count: Int = 0)
+
+		val names = HashMap<String, additiveStat>()
+		additives.forEach { additive ->
+			additive.description?.let { key ->
+				names.getOrPut(key, { additiveStat() }).apply {
+					total += additive.amount ?: 0.0
+					count++
+				}
+			}
+		}
+
+		names.forEach { (k, v) ->
+			val chip = Chip(context!!)
+			chip.text = k
+
+			additive_chips_container += chip
+		}
+	}
+
+	private fun renderStats(container: ViewGroup, templates: ArrayList<template>)
+	{
+		templates.forEach { template ->
+			var dataView = when (template)
+			{
+				is data -> {
+					LayoutInflater.from(activity).inflate(R.layout.data_label_stub, stats_container, false).also {
+						it.label.text = template.label
+						it.data.text = template.data
+					}
+				}
+
+				is header -> {
+					LayoutInflater.from(activity).inflate(R.layout.subtitle_stub, stats_container, false).also {
+						(it as TextView).text = template.label
+						it.setPadding(0, resources.getDimension(R.dimen.padding_16dp).toInt(), 0, 0)
+					}
+				}
+
+				else -> null
+			}
+
+			dataView ?: return
+			container += dataView
+		}
 	}
 }
