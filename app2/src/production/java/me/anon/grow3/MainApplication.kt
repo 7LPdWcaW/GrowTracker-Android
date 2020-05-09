2ce1a91237d17d0a3c78c22dfcff89c06d039aa5
7LPdWcaW <7LPdWcaW@gmail.com>
2020-05-09 08:42:43 +0100

    Bumps kotlin and fixes compile error

--- app2/src/production/java/me/anon/grow3/MainApplication.kt
+++ app2/src/production/java/me/anon/grow3/MainApplication.kt
@@ -4,3 +4,9 @@ package me.anon.grow3
  * // TODO: Add class description
  */
 class MainApplication : BaseApplication()
+{
+	override fun setup()
+	{
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+}
