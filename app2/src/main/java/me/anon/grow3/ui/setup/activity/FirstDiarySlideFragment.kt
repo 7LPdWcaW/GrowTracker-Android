a0ee336b69e7d5c579711960ffdf4a786cca67e4
7LPdWcaW <7LPdWcaW@gmail.com>
2020-05-15 20:26:06 +0100

    Adds custom intro page

---
+++ app2/src/main/java/me/anon/grow3/ui/setup/activity/FirstDiarySlideFragment.kt
@@ -0,0 +1,29 @@
+package me.anon.grow3.ui.setup.activity
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.appintro.SlideBackgroundColorHolder
+import kotlinx.android.synthetic.main.fragment_intro_slide_diary.*
+import me.anon.grow3.R
+import me.anon.grow3.util.resolve
+
+class FirstDiarySlideFragment : Fragment(), SlideBackgroundColorHolder
+{
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? = inflater.inflate(R.layout.fragment_intro_slide_diary, container, false)
+
+	override val defaultBackgroundColor: Int
+		get() = R.color.colorAccent.resolve(requireContext())
+
+	override fun setBackgroundColor(backgroundColor: Int)
+	{
+		content.setBackgroundColor(backgroundColor)
+	}
+}
