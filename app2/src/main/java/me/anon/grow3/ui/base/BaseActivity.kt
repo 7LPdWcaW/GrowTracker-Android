b1b14c33bda5414ec621ccb3f4a2d3ecc15e9405
7LPdWcaW <7LPdWcaW@gmail.com>
2020-05-23 22:21:03 +0100

    Adds base fragment class and layout res for base activity

--- app2/src/main/java/me/anon/grow3/ui/base/BaseActivity.kt
+++ app2/src/main/java/me/anon/grow3/ui/base/BaseActivity.kt
@@ -1,5 +1,29 @@
 package me.anon.grow3.ui.base
 
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.include_toolbar.view.*
+import me.anon.grow3.R
 
-open class BaseActivity : AppCompatActivity()
+open class BaseActivity(val layoutRes: Int = -1) : AppCompatActivity()
+{
+	protected var toolbar: Toolbar? = null
+
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+
+		if (layoutRes != -1) setContentView(layoutRes)
+	}
+
+	override fun setContentView(layoutResID: Int)
+	{
+		super.setContentView(layoutResID)
+
+		findViewById<View?>(R.id.include_toolbar)?.let {
+			toolbar = it.toolbar
+		}
+	}
+}
---
+++ app2/src/main/java/me/anon/grow3/ui/base/BaseFragment.kt
@@ -0,0 +1,27 @@
+package me.anon.grow3.ui.base
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import me.anon.grow3.di.ApplicationComponent
+import me.anon.grow3.util.component
+
+abstract class BaseFragment(layoutRes: Int) : Fragment(layoutRes)
+{
+	abstract val inject: (ApplicationComponent) -> Unit
+
+	override fun onAttach(context: Context)
+	{
+		super.onAttach(context)
+		inject(component)
+	}
+
+	override fun onActivityCreated(savedInstanceState: Bundle?)
+	{
+		super.onActivityCreated(savedInstanceState)
+
+		bindUi()
+	}
+
+	abstract fun bindUi()
+}
