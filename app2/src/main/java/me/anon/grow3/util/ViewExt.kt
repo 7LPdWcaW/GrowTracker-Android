b77212addf3c860723de5b8c143adf97e143f508
7LPdWcaW <7LPdWcaW@gmail.com>
2020-06-12 16:10:34 +0100

    Renames viewutils class and removes unused scroll view

--- app2/src/main/java/me/anon/grow3/util/ViewExt.kt
+++ app2/src/main/java/me/anon/grow3/util/ViewExt.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.annotation.*
+import androidx.core.view.*
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -180,6 +181,32 @@ inline fun <T : View> T.afterMeasured(crossinline callback: T.() -> Unit)
 			}
 		}
 	})
+}
+
+public fun View.addSystemWindowInsetToPadding(left: Boolean = false, top: Boolean = false, right: Boolean = false, bottom: Boolean = false)
+{
+	val (initialLeft, initialTop, initialRight, initialBottom) = listOf(paddingLeft, paddingTop, paddingRight, paddingBottom)
+
+	ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+		view.updatePadding(left = initialLeft + (if (left) insets.systemWindowInsetLeft else 0), top = initialTop + (if (top) insets.systemWindowInsetTop else 0), right = initialRight + (if (right) insets.systemWindowInsetRight else 0), bottom = initialBottom + (if (bottom) insets.systemWindowInsetBottom else 0))
+
+		insets
+	}
+}
+
+public fun View.addSystemWindowInsetToMargin(left: Boolean = false, top: Boolean = false, right: Boolean = false, bottom: Boolean = false)
+{
+	val (initialLeft, initialTop, initialRight, initialBottom) = listOf(marginLeft, marginTop, marginRight, marginBottom)
+
+	ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+		view.updateLayoutParams {
+			(this as? ViewGroup.MarginLayoutParams)?.let {
+				updateMargins(left = initialLeft + (if (left) insets.systemWindowInsetLeft else 0), top = initialTop + (if (top) insets.systemWindowInsetTop else 0), right = initialRight + (if (right) insets.systemWindowInsetRight else 0), bottom = initialBottom + (if (bottom) insets.systemWindowInsetBottom else 0))
+			}
+		}
+
+		insets
+	}
 }
 
 /**
