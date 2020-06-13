a023e37070eeb01073d13182b2cc56c01a5acc20
7LPdWcaW <7LPdWcaW@gmail.com>
2020-06-13 16:03:13 +0100

    Adds basic implementation for viewbinding classes

--- app2/src/main/java/me/anon/grow3/ui/base/BaseFragment.kt
+++ app2/src/main/java/me/anon/grow3/ui/base/BaseFragment.kt
@@ -2,13 +2,44 @@ package me.anon.grow3.ui.base
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 import me.anon.grow3.di.ApplicationComponent
 import me.anon.grow3.util.component
 
-abstract class BaseFragment(layoutRes: Int) : Fragment(layoutRes)
+abstract class BaseFragment : Fragment
 {
+	constructor() : super()
+	constructor(layoutRes: Int) : super(layoutRes)
+	constructor(viewBinder: Class<out ViewBinding>)
+	{
+		this._viewBinder = viewBinder
+	}
+
+	private var _viewBinder: Class<out ViewBinding>? = null
+	public lateinit var viewBinder: ViewBinding private set
+
+	public inline fun <reified T> binding(): T = viewBinder as T
+
 	abstract val inject: (ApplicationComponent) -> Unit
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+	{
+		if (_viewBinder == null)
+		{
+			return super.onCreateView(inflater, container, savedInstanceState)
+		}
+		else
+		{
+			viewBinder = _viewBinder!!.getDeclaredMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
+				.invoke(_viewBinder, inflater, container, false) as ViewBinding
+			return viewBinder.root
+		}
+	}
 
 	override fun onAttach(context: Context)
 	{
@@ -28,4 +59,9 @@ abstract class BaseFragment(layoutRes: Int) : Fragment(layoutRes)
 	open fun bindArguments(bundle: Bundle?) {}
 	abstract fun bindUi()
 	abstract fun bindVm()
+
+	public fun setToolbar(toolbar: Toolbar)
+	{
+		(activity as BaseActivity).setSupportActionBar(toolbar)
+	}
 }
--- app2/src/main/java/me/anon/grow3/ui/diaries/fragment/ViewDiaryFragment.kt
+++ app2/src/main/java/me/anon/grow3/ui/diaries/fragment/ViewDiaryFragment.kt
@@ -3,58 +3,42 @@ package me.anon.grow3.ui.diaries.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
-import androidx.core.view.isVisible
-import kotlinx.android.synthetic.main.fragment_view_diary.*
-import me.anon.grow3.R
+import me.anon.grow3.databinding.FragmentViewDiaryBinding
 import me.anon.grow3.di.ApplicationComponent
 import me.anon.grow3.ui.base.BaseFragment
-import me.anon.grow3.ui.crud.activity.DiaryActivity
 import me.anon.grow3.ui.main.activity.MainActivity
-import me.anon.grow3.util.navigateForResult
+import me.anon.grow3.util.applyWindowInsets
 import me.anon.grow3.util.navigateTo
 import me.anon.grow3.util.onClick
 
-class ViewDiaryFragment : BaseFragment(R.layout.fragment_view_diary)
+class ViewDiaryFragment : BaseFragment(FragmentViewDiaryBinding::class.java)
 {
 	companion object
 	{
 		public const val EXTRA_DIARY_ID = "diary.id"
 	}
 
-	private lateinit var diaryId: String
 	override val inject: (ApplicationComponent) -> Unit = {}
+	private val viewBindings by lazy { binding<FragmentViewDiaryBinding>() }
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?)
 	{
 		super.onViewCreated(view, savedInstanceState)
-		activity?.actionBar?.hide()
+		setToolbar(viewBindings.toolbar)
+		applyWindowInsets()
 	}
 
 	override fun bindArguments(bundle: Bundle?)
 	{
-		if (arguments?.getString(EXTRA_DIARY_ID).isNullOrBlank())
-		{
-			empty_view.isVisible = true
-			diary_details.isVisible = false
-			new_diary.onClick {
-				navigateForResult<DiaryActivity>()
-			}
-		}
-		else
-		{
-			empty_view.isVisible = false
-			diary_details.isVisible = true
-			requireActivity().title = "Diary"
-			diaryId = arguments?.getString(EXTRA_DIARY_ID)!!
-		}
+		requireActivity().title = "Diary"
 	}
 
 	override fun bindUi()
 	{
-		crop1.onClick {
+		viewBindings.crop1.onClick {
 			navigateTo<MainActivity> {
 				putExtras(bundleOf(MainActivity.EXTRA_NAVIGATE to MainActivity.NAVIGATE_TO_CROPS))
-				putExtras(bundleOf(MainActivity.EXTRA_DIARY_ID to diaryId))
+//				putExtras(bundleOf(MainActivity.EXTRA_DIARY_ID to diaryId))
 			}
 		}
 	}
