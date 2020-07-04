8ae9e097b95d9c392a98c12a6bc2e7bc13271e32
7LPdWcaW <7LPdWcaW@gmail.com>
2020-07-04 19:55:16 +0100

    Genericises the operator override

--- app2/src/main/java/me/anon/grow3/util/Duo.kt
+++ app2/src/main/java/me/anon/grow3/util/Duo.kt
@@ -1,16 +1,9 @@
 package me.anon.grow3.util
 
-import me.anon.grow3.data.model.Log
-import me.anon.grow3.data.model.MediumType
-import me.anon.grow3.data.model.Type
-
 class Duo<A>(
 	var first: A,
 	var second: A?
 )
 
 public infix fun <A> A.and(that: A?): Duo<A> = Duo(this, that)
-
-public operator fun Log.plus(second: Log): Duo<Log> = Duo(this, second)
-public operator fun Type.plus(second: Type): Duo<Type> = Duo(this, second)
-public operator fun MediumType.plus(second: MediumType): Duo<MediumType> = Duo(this, second)
+public operator fun <A> A.plus(second: A?): Duo<A> = Duo(this, second)
