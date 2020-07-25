8184aeae6b380863cd0e125125b8dfbb6133e8f6
7LPdWcaW <7LPdWcaW@gmail.com>
2020-07-25 19:21:09 +0100

    Adds basic settings activity

---
+++ app2/src/main/java/me/anon/grow3/ui/settings/activity/SettingsActivity.kt
@@ -0,0 +1,6 @@
+package me.anon.grow3.ui.settings.activity
+
+import me.anon.grow3.databinding.ActivitySettingsBinding
+import me.anon.grow3.ui.base.BaseActivity
+
+class SettingsActivity : BaseActivity(ActivitySettingsBinding::class)
