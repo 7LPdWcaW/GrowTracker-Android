b736c966ccf57b0623706c90eeaa8d3a50814d8e
7LPdWcaW <7LPdWcaW@gmail.com>
2021-10-24 20:12:13 +0100

    Adds transport log view

--- app2/src/main/java/me/anon/grow3/ui/action/view/TransplantLogView.kt
+++ app2/src/main/java/me/anon/grow3/ui/action/view/TransplantLogView.kt
@@ -3,10 +3,11 @@ package me.anon.grow3.ui.action.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import me.anon.grow3.data.model.Diary
-import me.anon.grow3.data.model.Log
-import me.anon.grow3.data.model.Transplant
+import me.anon.grow3.data.model.*
 import me.anon.grow3.databinding.FragmentActionLogTransplantBinding
+import me.anon.grow3.util.asEditable
+import me.anon.grow3.util.asStringOrNull
+import me.anon.grow3.util.toDoubleOrNull
 
 class TransplantLogView : LogView<FragmentActionLogTransplantBinding>
 {
@@ -28,14 +29,41 @@ class TransplantLogView : LogView<FragmentActionLogTransplantBinding>
 
 	override fun bind(view: FragmentActionLogTransplantBinding)
 	{
+		view.mediumTypeOptions.singleSelection = true
+		view.mediumTypeOptions.setMenu(MediumType.toMenu())
+		view.mediumTypeOptions.itemSelectListener = { item ->
+			log.medium = MediumType.ofId(item.itemId)
+		}
 
+		view.mediumSizeUnitOptions.singleSelection = true
+		view.mediumSizeUnitOptions.setMenu(VolumeUnit.toMenu())
+		view.mediumSizeUnitOptions.itemSelectListener = { item ->
+			view.mediumSize.editText!!.text.toDoubleOrNull()?.let { amount ->
+				log.size = Volume(amount, VolumeUnit.ofId(item.itemId))
+			}
+		}
+
+		view.mediumTypeOptions.checkItems(log.medium.strRes)
+		log.size?.let { size ->
+			view.mediumSizeUnitOptions.checkItems(size.unit.strRes)
+			view.mediumSize.editText!!.text = size.amount.asStringOrNull()?.asEditable()
+		}
 	}
 
 	override fun save(view: FragmentActionLogTransplantBinding): Log
 	{
 		view.root.clearFocus()
+		val type = view.mediumTypeOptions.getSelectedItems().firstOrNull()
+		val sizeUnit = view.mediumSizeUnitOptions.getSelectedItems().firstOrNull()
+		val size = view.mediumSize.editText!!.text.toDoubleOrNull()
+
+		log.apply {
+			this.medium = MediumType.ofId(type!!.itemId)
+			this.size = Volume(size!!, VolumeUnit.ofId(sizeUnit!!.itemId))
+		}
+
 		return log
 	}
 
-	override fun provideTitle(): String = ""
+	override fun provideTitle(): String = "Medium transplant"
 }
